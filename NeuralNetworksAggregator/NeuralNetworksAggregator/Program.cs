ab0a7a8751bd8d85a8551b6d85f5d3b5cff93f15
animolos <56922482+animolos@users.noreply.github.com>
2021-01-24 14:17:41 +0500

    Add files via upload

---
+++ NeuralNetworksAggregator/NeuralNetworksAggregator/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Threading;
+using System.Threading.Tasks;
+using NeuralNetworksAggregator.Application;
+using NeuralNetworksAggregator.Application.BotApplication;
+using NeuralNetworksAggregator.Application.CmdApplication;
+using Telegram.Bot;
+using Telegram.Bot.Args;
+using Telegram.Bot.Types;
+using Telegram.Bot.Types.Enums;
+using Telegram.Bot.Types.InputFiles;
+using Telegram.Bot.Types.ReplyMarkups;
+
+namespace NeuralNetworksAggregator
+{
+    internal class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            Trace.Listeners.Add(new ConsoleTraceListener {Filter = new EventTypeFilter(SourceLevels.Error)});
+            Trace.Listeners.Add(new ConsoleTraceListener {Filter = new EventTypeFilter(SourceLevels.Information)});
+            // Trace.Listeners.Add(..)
+
+            await BotApplication.Run(args);
+        }
+
+        public static void MainX(string[] args)
+        {
+            CmdApplication.Run(args);
+        }
+    }
+}
